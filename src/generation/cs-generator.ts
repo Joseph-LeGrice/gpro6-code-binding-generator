191e0fc506723104da4eca9a68b11a810b5836ce
Joseph Le Grice <joe.legrice@gmail.com>
2018-05-01 14:59:42 +0100

    implemented cs generator

--- src/generation/cs-generator.ts
+++ src/generation/cs-generator.ts
@@ -1,28 +1,43 @@
 import { GenerationConfig } from "../config/generation-config";
+import { MethodConfig } from "../config/method-config";
+import { GeneratedType } from "../config/argument-info";
 
-class CSGenerator 
+export class CsGenerator 
 {
     constructor(public config: GenerationConfig) { }
 
-    private csFileTemplate(generatedMethods: string): string {
-        return `
-        //// GENERATED
-        ${generatedMethods}
-        //// GENERATED`
+    public csFileTemplate(): string {
+        const result: Array<string> = new Array<string>();
+        result.push(`class ${this.config.className(GeneratedType.cs)}`);
+        result.push(`{`);
+        result.push(`\t//// GENERATED`)
+        for (const m of this.config.allMethods) {
+            if (m.type === 'instance') {
+                result.push(`${this.csInstanceMethod(m)}\n`);
+            } else if (m.type === 'static') {
+                result.push(`${this.csStaticMethod(m)}\n`);
+            }
+        }
+        result.push(`\t//// GENERATED`);
+        result.push(`}`);
+        return result.join('\n');
     }
     
-    private csInstanceMethod(): string {
-        return `[MethodImpl(MethodImplOptions.InternalCall)]  
-        private extern static [MANAGED_RETURN_TYPE] [MANAGED_METHOD_NAME](int instanceid, [MANAGED_METHOD_ARGS]);  
-    
-        public [MANAGED_RETURN_TYPE] [MANAGED_METHOD_NAME]([MANAGED_METHOD_ARGS])  
-        {  
-            [MANAGED_METHOD_NAME](InstanceID, [MANAGED_METHOD_ARGS]);  
-        }`
+    private csInstanceMethod(method: MethodConfig): string {
+        const result: Array<string> = new Array<string>();
+        result.push(`\t[MethodImpl(MethodImplOptions.InternalCall)]`);
+        result.push(`\tprivate extern static ${method.returnType(GeneratedType.cs)} ${method.name}(int instanceid, ${method.getArgs(GeneratedType.cs)});`);
+        result.push(`\tpublic ${method.returnType(GeneratedType.cs)} ${method.name}(${method.getArgs(GeneratedType.cs)})`);
+        result.push(`\t{`);
+        result.push(`\t\t${method.name}(InstanceID, ${method.getArgs(GeneratedType.cs)});`);
+        result.push(`\t}`);
+        return result.join('\n');
     }
     
-    private csStaticMethod(): string {
-        return `[MethodImplAttribute(MethodImplOptions.InternalCall)]  
-        [VISIBILITY] extern static [MANAGED_RETURN_TYPE] [MANAGED_METHOD_NAME]([MANAGED_METHOD_ARGS]);`
+    private csStaticMethod(method: MethodConfig): string {
+        const result: Array<string> = new Array<string>()
+        result.push(`\t[MethodImplAttribute(MethodImplOptions.InternalCall)]`);
+        result.push(`\tpublic extern static ${method.returnType(GeneratedType.cs)} ${method.name}(${method.getArgs(GeneratedType.cs)});`);
+        return result.join('\n');
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { ArgumentParser } from 'argparse'
 import * as fs from 'fs-extra'
 import { GenerationConfig } from './config/generation-config';
 import { CppGenerator } from './generation/cpp-generator';
+import { CsGenerator } from './generation/cs-generator';
 
 var parser = new ArgumentParser();
 parser.addArgument(['FILE']);
@@ -11,6 +12,9 @@ var json = fs.readJsonSync(args.FILE);
 const config = new GenerationConfig(json);
 console.log(config);
 
-const gen = new CppGenerator(config);
-console.log(gen.cppHeaderFileTemplate());
-console.log(gen.cppSourceFileTemplate());
+const cppGen = new CppGenerator(config);
+console.log(cppGen.cppHeaderFileTemplate());
+console.log(cppGen.cppSourceFileTemplate());
+
+const csGen = new CsGenerator(config);
+console.log(csGen.csFileTemplate());
